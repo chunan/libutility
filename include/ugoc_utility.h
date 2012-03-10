93c72c02fc717c533e6049248e385b4d2a265de9
ugoc <ugocugoc@gmail.com>
2012-03-10 11:31:19 +0800

    Add class Timer, TwoDimArray
    - Timer is a handy class for time tracking
    - TwoDimArray is a warping class for two dimensional array
      while a one dimension structure must be maintained
      (unlike vector<vecotr<_Tp> >)

--- include/ugoc_utility.h
+++ include/ugoc_utility.h
@@ -2,11 +2,43 @@
 #define UTILITY_H
 
 #include <sys/times.h>
+#include <cassert>
 #include <cstdio>
+#include <deque>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <iostream>
 
-using namespace std;
+using std::cout;
+using std::endl;
+using std::setw;
+using std::deque;
+using std::vector;
+using std::string;
 
 char *Strcpy(char *&dest, const char src[]);
+
+template <class _Tp>
+inline _Tp Min(const _Tp a, const _Tp b)/*{{{*/
+{
+	if(a <= b) return a;
+	else return b;
+}
+/*}}}*/
+
+template <typename _Tp>
+typename deque<_Tp>::iterator Max(deque<_Tp> &q, int size) {/*{{{*/
+	typename deque<_Tp>::iterator maxItr = q.begin();
+	for(typename deque<_Tp>::iterator it = q.begin()+1; it != q.end(); it++){
+		size--;
+		if(size == 0) break;
+		if(*it > *maxItr){
+			maxItr = it;
+		}
+	}
+	return maxItr;
+}/*}}}*/
 
 template <class _Tp>
 class mem_op {/*{{{*/
@@ -66,14 +98,106 @@ bool Free_1d_array(_Tp *&ptr)
 	return true;
 }/*}}}*/
 
-double CalCpuTime(clock_t real, struct tms *tms_start, struct tms * tms_end, bool print);
+/* Maitain a 2D array stored as an 1D array (continuous space)
+ * R(): current number of rows
+ * C(): current number of columns
+ * Resize(nr, nc): resize to nr-by-nc array
+ * Memfill(val): fill the array with val
+ * (r, c): return the (r, c) entry (reference to entry)
+ * [r]: return the [r] vector (pointer of (r, 0))
+ */
+template<class _Tp>
+class TwoDimArray {
+  public:
+    TwoDimArray() { Init(); }
+    TwoDimArray(int nr, int nc) {
+      Init();
+      Resize(nr, nc);
+    }
+    ~TwoDimArray() {
+      mem_op<_Tp>::delete_2d_array(&data_);
+    }
+    void Resize(const int nr, const int nc) {
+      nr_ = nr;
+      nc_ = nc;
+      mem_op<_Tp>::reallocate_2d_array(&data_, nr, nc,
+                                       &nr_max_, &nc_max_, &size_);
+    }
+    void Memfill(const _Tp& val) {
+      if (data_ != NULL) {
+        for (int i = 0; i < nr_ * nc_; ++i) {
+          data_[0][i] = val;
+        }
+      }
+    }
+    _Tp& operator()(const int r, const int c) {
+      assert(data_ != NULL);
+      assert(r >= 0 && r < nr_);
+      assert(c >= 0 && c < nc_);
+      return data_[r][c];
+    }
+    _Tp Entry(const int r, const int c) const {
+      assert(data_ != NULL);
+      assert(r >= 0 && r < nr_);
+      assert(c >= 0 && c < nc_);
+      return data_[r][c];
+    }
+    _Tp* operator[](const int r) {
+      if (data_ == NULL) {
+        return NULL;
+      } else {
+        assert(r >= 0 && r < nr_);
+        return data_[r];
+      }
+    }
+    const _Tp* Vec(const int r) const {
+      if (data_ == NULL) {
+        return NULL;
+      } else {
+        assert(r >= 0 && r < nr_);
+        return data_[r];
+      }
+    }
+    int R() const { return nr_; }
+    int C() const { return nc_; }
+    int R_max() const { return nr_max_; }
+    int C_max() const { return nc_max_; }
+    void DumpData() const {}
+  private:
+    void Init() {
+      data_ = NULL;
+      nr_ = nc_ = nr_max_ = nc_max_ = size_ = 0;
+    }
+    _Tp** data_;
+    int nr_;
+    int nc_;
+    int nr_max_;
+    int nc_max_;
+    int size_;
+};
 
-void TicTimer(clock_t &start_t_i, struct tms &tms_start, bool &timeron);
+void CalCpuTime(clock_t real, struct tms *tms_start, struct tms * tms_end,
+                double *rtime, double *utime, double *stime,
+                bool print);
 
-void TocTimer(clock_t &end_t_i, struct tms &tms_end, bool &timeron);
+clock_t TimeStamp(struct tms *tms_stamp, bool *timeron);
 
 FILE *FOPEN(const char fname[], char const flag[]);
 
 void ErrorExit(const char file[], int line, int exit_value, const char *format, ...);
+
+class Timer {
+  public:
+    Timer() { is_working = true; }
+    void Print();
+    unsigned Tic(const char* msg);
+    void Toc(unsigned i);
+  private:
+    vector<string> log;
+    vector<struct tms> s_stamp, e_stamp;
+    vector<clock_t> s_real, e_real;
+    vector<bool> tic_not_toc;
+    bool is_working;
+};
 
 #endif
--- src/ugoc_utility.cpp
+++ src/ugoc_utility.cpp
@@ -2,8 +2,13 @@
 #include <cstdarg>
 #include <cstdlib>
 #include <string.h>
+#include <iostream>
 
 #include "ugoc_utility.h"
+
+using std::cout;
+using std::cerr;
+using std::endl;
 
 FILE *FOPEN(const char fname[], char const flag[])
 {/*{{{*/
@@ -15,10 +20,9 @@ FILE *FOPEN(const char fname[], char const flag[])
 	return fd;
 }/*}}}*/
 
-char *Strcpy(char *&dest, const char src[])
-{/*{{{*/
+char *Strcpy(char *&dest, const char src[]) {/*{{{*/
 	if(dest != NULL) Free_1d_array(dest);
-	dest = new char [strlen(src)+1];
+	dest = new char [strlen(src) + 1];
 	strcpy(dest,src);
 	return dest;
 }/*}}}*/
@@ -37,35 +41,83 @@ void ErrorExit(const char file[], int line, int exit_value, const char *format, 
 
 }/*}}}*/
 
-double CalCpuTime(clock_t real, struct tms *tms_start, struct tms * tms_end, bool print) {
+void CalCpuTime(clock_t real, struct tms *tms_start, struct tms * tms_end,
+                double *rtime, double *utime, double *stime,
+                bool print) {
 	static long clktck = 0;
 	if (clktck == 0){
 		if ((clktck = sysconf(_SC_CLK_TCK)) < 0){
 			fprintf(stderr,"Warning: line %d: sysconf error\n",__LINE__);
-			return -1;
+			return;
 		}
 	}
-	double utime = (tms_end->tms_utime - tms_start->tms_utime) / (double) clktck;
-	double stime = (tms_end->tms_stime - tms_start->tms_stime) / (double) clktck;
-	if (print){
-		fprintf(stdout," real  %7.2f\n", real/(double) clktck);
-		fprintf(stdout," user: %7.2f\n", utime);
-		fprintf(stdout," sys:  %7.2f\n", stime);
+  *rtime = real/(double) clktck;
+	*utime = (tms_end->tms_utime - tms_start->tms_utime) / (double) clktck;
+	*stime = (tms_end->tms_stime - tms_start->tms_stime) / (double) clktck;
+	if (print) {
+		fprintf(stdout," real  %7.2f\n", *rtime);
+		fprintf(stdout," user: %7.2f\n", *utime);
+		fprintf(stdout," sys:  %7.2f\n", *stime);
 	}
-	return utime + stime;
 }
 
-void TicTimer(clock_t &start_t_i, struct tms &tms_start, bool &timeron) {
-  if ((start_t_i = times(&tms_start)) == -1) {
-    timeron = false;
+clock_t TimeStamp(struct tms *tms_stamp, bool *timeron) {
+  clock_t real_t;
+  if (*timeron == false) {
+    return -1;
+  } else if ((real_t = times(tms_stamp)) == -1) {
+    *timeron = false;
     fprintf(stderr,"Warning: %s, line %d: times error\n",__FILE__,__LINE__);
+    return -1;
   }
+  return real_t;
 }
 
-void TocTimer(clock_t &end_t_i, struct tms &tms_end, bool &timeron) {
-  if (timeron && (end_t_i = times(&tms_end)) == -1) {
-    timeron = false;
-    fprintf(stderr,"Warning: %s, line %d: times error\n",__FILE__,__LINE__);
+void Timer::Print() {
+  double rtime, utime, stime;
+  double sum_rtime = 0, sum_utime = 0, sum_stime = 0;
+  cout << "ID    real    user     sys     log\n";
+  cout << "-----------------------------------\n";
+  for (unsigned i = 0; i < log.size(); ++i) {
+    if (tic_not_toc[i]) {
+      cout << "Timer is tic but not toc.";
+    } else {
+      CalCpuTime(e_real[i] - s_real[i],
+                 &s_stamp[i], &e_stamp[i],
+                 &rtime, &utime, &stime, false);
+      cout << setw(2) << i 
+        << setw(8) << rtime
+        << setw(8) << utime
+        << setw(8) << stime;
+      sum_rtime += rtime;
+      sum_utime += utime;
+      sum_stime += stime;
+    }
+    cout << setw(8) << log[i] << endl;
   }
+  cout << "------------------------------------\n";
+  cout << "Sum"
+    << setw(7) << sum_rtime
+    << setw(8) << sum_utime
+    << setw(8) << sum_stime
+    << setw(8) << sum_utime + sum_stime << "\n";
 }
 
+unsigned Timer::Tic(const char* msg) {
+  unsigned i = s_stamp.size();
+  log.push_back(msg);
+  s_stamp.push_back(tms());
+  e_stamp.push_back(tms());
+  s_real.push_back(TimeStamp(&s_stamp[i], &is_working));
+  e_real.push_back(0);
+  tic_not_toc.push_back(true);
+  return i;
+}
+
+void Timer::Toc(unsigned i) {
+  if (!tic_not_toc[i]) {
+    cerr << "Warning: timer[" << i << "] re-stamped, overwrite.\n";
+  }
+  e_real[i] = TimeStamp(&e_stamp[i], &is_working);
+  tic_not_toc[i] = false;
+}
